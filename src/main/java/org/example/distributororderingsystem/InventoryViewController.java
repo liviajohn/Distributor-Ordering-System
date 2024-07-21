5afc5a4e18c9f69e6d5c941708f3370d9ae3313f
brleeatlanta <lee.bethany.15@gmail.com>
2024-07-21 15:19:34 -0400

    Update InventoryViewController.java
    Changed Texts to ListView like in OrderFormController to try to make product info show up

--- src/main/java/org/example/distributororderingsystem/InventoryViewController.java
+++ src/main/java/org/example/distributororderingsystem/InventoryViewController.java
@@ -1,48 +1,39 @@
 package org.example.distributororderingsystem;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class InventoryViewController {
 
     @FXML
-    private Text id;
+    private ListView<Text> productList = new ListView<>();
     @FXML
-    private Text productName;
-    @FXML
-    private Text brandName;
-    @FXML
-    private Text size;
-    @FXML
-    private Text productClass;
-    @FXML
-    private Text amountAvailable;
+    private ObservableList<Text> products;
     @FXML
     private Button addToCart;
     @FXML
     private Button Checkout;
     @FXML
-    private ImageView backArrow;
+    private Button back;
 
     @FXML
     protected void initialize() {
-        id.setText(ProductManagement.getIDList(15));
-        productName.setText(ProductManagement.getNameList(15));
-        brandName.setText(ProductManagement.getBrandList(15));
-        size.setText(ProductManagement.getSizeList(15));
-        productClass.setText(ProductManagement.getClassList(15));
-        amountAvailable.setText(ProductManagement.getAvailableList(15));
+        products = FXCollections.observableArrayList(ProductManagement.getProductInfo(15));
+        productList.setItems(products);
     }
 
     @FXML
-    protected void onBackArrowClick(ActionEvent event) throws Exception {
+    protected void onBackButtonClick(ActionEvent event) throws Exception {
         // Go back to the Order Form
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/distributororderingsystem/order-form-view.fxml"));
